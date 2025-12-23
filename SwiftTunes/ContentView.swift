9923bf29d37a94a61569d3083a765bf5a554aba7
lakshanpd <lakshanpd.cse21@gmail.com>
2025-12-23 12:15:55 +0530

    Initial Commit

---
+++ SwiftTunes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTunes
+//
+//  Created by Danuka Lakshan on 2025-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTunes/SwiftTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTunesApp.swift
+//  SwiftTunes
+//
+//  Created by Danuka Lakshan on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
